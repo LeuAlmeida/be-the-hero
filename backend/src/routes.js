4ae9c0ad9fc12e125de4d805d72c07f8a693b5eb
Léu Almeida <leo@webid.net.br>
2020-03-25 17:28:23 -0300

    feat(routes): route to create ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res) => {
-  res.json({
-    evento: 'Semana Omnistack 11',
-    aluno: 'Léu Almeida',
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
   });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
