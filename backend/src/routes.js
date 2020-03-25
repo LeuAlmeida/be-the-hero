0669439bdccb10bd92b76d00651cff44941ce68e
Léu Almeida <leo@webid.net.br>
2020-03-25 17:53:47 -0300

    refactor(routes): transform post method in a separated controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const crypto = require('crypto');
 const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
 
 const routes = express.Router();
 
@@ -10,21 +11,6 @@ routes.get('/ongs', async (req, res) => {
   return res.json(ongs);
 });
 
-routes.post('/ongs', async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return res.json({ id });
-});
+routes.post('/ongs', OngController.store);
 
 module.exports = routes;
