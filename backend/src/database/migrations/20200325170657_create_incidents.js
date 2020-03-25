72ff330c92e0a190f09de433e6e2bc0ab8c06ffd
Léu Almeida <leo@webid.net.br>
2020-03-25 17:11:21 -0300

    feat(migration): improving incidents migration

--- backend/src/database/migrations/20200325170657_create_incidents.js
+++ backend/src/database/migrations/20200325170657_create_incidents.js
@@ -1,8 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('incidents', function (table) {
+    table.increments();
 
-exports.up = function(knex) {
-  
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ong_id').notNullable();
+
+    table.foreign('ong_id').references('id').inTable('ongs');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('incidents');
 };
