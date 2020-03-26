0f5554fa7f9e5505f5e22b000b9a8ba7743794ac
Léu Almeida <leo@webid.net.br>
2020-03-26 18:01:37 -0300

    typo(Register): update back-home text

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -21,7 +21,7 @@ export default function Register() {
 
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
-            Não tenho cadastro
+            Voltar para home
           </Link>
         </section>
 
