d0476a8014e90babc3b361b104688b50c10486b7
Léu Almeida <leo@webid.net.br>
2020-03-26 14:02:53 -0300

    feat(routes): create routes to log in

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const SessionController = require('./controllers/SessionController');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
