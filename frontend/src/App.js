831074386ee6d8e8950bd3cd6fc5b2a1501a7173
Léu Almeida <leo@webid.net.br>
2020-03-26 16:54:02 -0300

    refactor(App): improving routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './global.css';
 
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 export default function App() {
-  return <Logon />;
+  return <Routes />;
 }
