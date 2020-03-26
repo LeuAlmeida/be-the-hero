dc90d7a19c9befe8ffdbf2616766ad3cae31af9c
Léu Almeida <leo@webid.net.br>
2020-03-26 13:50:46 -0300

    feat(controller/IncidentController): improving delete method

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -9,7 +9,6 @@ module.exports = {
 
   async store(req, res) {
     const { title, description, value } = req.body;
-
     const ong_id = req.headers.authorization;
 
     const [id] = await connection('incidents').insert({
@@ -20,5 +19,23 @@ module.exports = {
     });
 
     return res.json({ id });
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' });
+    }
+
+    await connection('incidents').where('id', id).delete();
+
+    return res.status(204).send();
   },
 };
